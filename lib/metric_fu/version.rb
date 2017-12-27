3df3e01de62a2ab053d180a4cb0f92357395c70f
RainChen <hirainchen@gmail.com>
2017-12-27 17:15:57 +0800

    bump version to 4.14.3

--- lib/metric_fu/version.rb
+++ lib/metric_fu/version.rb
@@ -2,7 +2,7 @@ module MetricFu
   class Version
     MAJOR = "4"
     MINOR = "14"
-    PATCH = "2"
+    PATCH = "3"
     PRE   = ""
   end
   VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH].join(".")
